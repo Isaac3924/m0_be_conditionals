095e29a3527914de7d4ecf00ba07ec6841ff22e2
Isaac Alter <i_alter@yahoo.com>
2022-11-02 13:09:56 -0600

    Adds third coding challenge

---
+++ code_challenges/fizzbuzz.rb
@@ -0,0 +1,47 @@
+fb = 15
+
+if fb % 5 == 0 && fb % 3 == 0
+    p "FizzBuzz"
+elsif fb % 5 != 0 && fb % 3 == 0
+    p "Fizz"
+elsif fb % 5 == 0 && fb % 3 != 0
+    p "Buzz"
+else
+    p fb
+end
+
+fb = 9
+
+if fb % 5 == 0 && fb % 3 == 0
+    p "FizzBuzz"
+elsif fb % 5 != 0 && fb % 3 == 0
+    p "Fizz"
+elsif fb % 5 == 0 && fb % 3 != 0
+    p "Buzz"
+else
+    p fb
+end
+
+fb = 20
+
+if fb % 5 == 0 && fb % 3 == 0
+    p "FizzBuzz"
+elsif fb % 5 != 0 && fb % 3 == 0
+    p "Fizz"
+elsif fb % 5 == 0 && fb % 3 != 0
+    p "Buzz"
+else
+    p fb
+end
+
+fb = 14
+
+if fb % 5 == 0 && fb % 3 == 0
+    p "FizzBuzz"
+elsif fb % 5 != 0 && fb % 3 == 0
+    p "Fizz"
+elsif fb % 5 == 0 && fb % 3 != 0
+    p "Buzz"
+else
+    p fb
+end
